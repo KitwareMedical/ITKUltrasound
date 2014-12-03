27d0da07de0aade1dad092cc893a26a83217b8c2
Matt McCormick <matt.mccormick@kitware.com>
2014-12-03 14:12:46 -0500

    ENH: Add BMode baseline comparison.

--- test/itkBModeImageFilterTestTiming.cxx
+++ test/itkBModeImageFilterTestTiming.cxx
@@ -4,18 +4,21 @@
 #include "itkImageFileReader.h"
 #include "itkIntensityWindowingImageFilter.h"
 #include "itkTimeProbe.h"
+#include "itkImageFileWriter.h"
 
 #include "itkBModeImageFilter.h"
 
 int itkBModeImageFilterTestTiming( int argc, char* argv[] )
 {
-  if( argc < 2 )
+  if( argc < 3 )
     {
     std::cerr << "Usage: " << argv[0];
-    std::cerr << " inputImage";
+    std::cerr << " inputImage outputImage";
     std::cerr << std::endl;
     return EXIT_FAILURE;
     }
+  const char * inputImageFileName = argv[1];
+  const char * outputImageFileName = argv[2];
 
   typedef float PixelType;
   const unsigned int Dimension = 3;
@@ -29,7 +32,7 @@ int itkBModeImageFilterTestTiming( int argc, char* argv[] )
   BModeFilterType::Pointer bMode = BModeFilterType::New();
   WindowingType::Pointer window  = WindowingType::New();
 
-  reader->SetFileName( argv[1] );
+  reader->SetFileName( inputImageFileName );
 
   try
     {
@@ -61,6 +64,20 @@ int itkBModeImageFilterTestTiming( int argc, char* argv[] )
   double frame_rate = static_cast< double >( size[2] ) / clock.GetMean();
 
   std::cout << "Frame rate achieved over " << clock.GetNumberOfStarts() << " runs was " << frame_rate << " fp" << clock.GetUnit() << "." << std::endl;
+
+  typedef itk::ImageFileWriter< ImageType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName( outputImageFileName );
+  writer->SetInput( window->GetOutput() );
+  try
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & error )
+    {
+    std::cerr << "Error: " << error << std::endl;
+    return EXIT_FAILURE;
+    }
 
   return EXIT_SUCCESS;
 }
