fb4ff7773d68989986f716e78be00ac17abbb1aa
Matt McCormick <matt.mccormick@kitware.com>
2015-07-29 23:31:37 -0400

    ENH: Build outside of the ITK source tree.

--- itk-module.cmake
+++ itk-module.cmake
@@ -1,11 +1,17 @@
 set(DOCUMENTATION "The modules provides filters may be particularly useful for
 ultrasound image reconstruction and analysis.")
 
+# A library is only created when FFTW support is ON
+set(_fft_depends COMPILE_DEPENDS ITKFFT)
+if(ITK_USE_FFTWF OR ITK_USE_FFTWD)
+  set(_fft_depends ITKFFT)
+endif()
+
 itk_module(Ultrasound
   DEPENDS
-    ITKFFT
     ITKImageCompose
     ITKImageIntensity
+    ${_fft_depends}
   TEST_DEPENDS
     ITKTestKernel
   EXCLUDE_FROM_DEFAULT
