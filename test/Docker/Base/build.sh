30f10abe1f63b5a71eb80e7fcf9ff9252c70fb59
Matt McCormick <matt.mccormick@kitware.com>
2015-08-03 12:46:45 -0400

    ENH: Add CircleCI build configuration.

--- test/Docker/Base/build.sh
+++ test/Docker/Base/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build -t itkultrasound-base .
+docker build -t thewtex/itkultrasound-base .
---
+++ test/Docker/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This is a script to build the modules and run the test suite in the base
+# Docker container.
+
+die() {
+  echo "Error: $@" 1>&2
+  exit 1;
+}
+
+cd /usr/src/ITKUltrasound-build || die "Could not cd into the build directory"
+
+cmake \
+  -G Ninja \
+  -DCMAKE_BUILD_TYPE:STRING=Release \
+    /usr/src/ITKUltrasound || die "CMake configuration failed"
+ctest -D Experimental || die "ctest failed"
