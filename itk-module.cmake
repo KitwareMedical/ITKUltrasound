965b0903ba68b1e31a1110825e73abfbba4dd7f5
Matt McCormick <matt.mccormick@kitware.com>
2016-05-15 08:01:22 -0400

    COMP: Move ITKFFT to DEPENDS from COMPILE_DEPENDS
    To address:
    
      test/CMakeFiles/UltrasoundTestDriver.dir/itkAnalyticSignalImageFilterTest.cxx.o:
      In function `itk::FFTWForward1DFFTImageFilter<itk::Image<float, 2u>,
      itk::Image<std::complex<float>, 2u> >::~FFTWForward1DFFTImageFilter()':
      itkAnalyticSignalImageFilterTest.cxx:(.text._ZN3itk27FFTWForward1DFFTImageFilterINS_5ImageIfLj2EEENS1_ISt7complexIfELj2EEEED2Ev[_ZN3itk27FFTWForward1DFFTImageFilterINS_5ImageIfLj2EEENS1_ISt7complexIfELj2EEEED5Ev]+0xbd):
      undefined reference to `fftwf_destroy_plan'
      test/CMakeFiles/UltrasoundTestDriver.dir/itkAnalyticSignalImageFilterTest.cxx.o:
      In function
      `itk::FFTWComplexToComplex1DFFTImageFilter<itk::Image<std::complex<float>,
      2u>, itk::Image<std::complex<float>, 2u>
      >::~FFTWComplexToComplex1DFFTImageFilter()':
      itkAnalyticSignalImageFilterTest.cxx:(.text._ZN3itk36FFTWComplexToComplex1DFFTImageFilterINS_5ImageISt7complexIfELj2EEES4_ED2Ev[_ZN3itk36FFTWComplexToComplex1DFFTImageFilterINS_5ImageISt7complexIfELj2EEES4_ED5Ev]+0xbd):
      undefined reference to `fftwf_destroy_plan'
      test/CMakeFiles/UltrasoundTestDriver.dir/itkAnalyticSignalImageFilterTest.cxx.o:
      In function
      `itk::FFTWComplexToComplex1DFFTImageFilter<itk::Image<std::complex<float>,
      2u>, itk::Image<std::complex<float>, 2u>
      >::~FFTWComplexToComplex1DFFTImageFilter()':
      itkAnalyticSignalImageFilterTest.cxx:(.text._ZN3itk36FFTWComplexToComplex1DFFTImageFilterINS_5ImageISt7complexIfELj2EEES4_ED0Ev[_ZN3itk36FFTWComplexToComplex1DFFTImageFilterINS_5ImageISt7complexIfELj2EEES4_ED5Ev]+0xc5):
      undefined reference to `fftwf_destroy_plan'

--- itk-module.cmake
+++ itk-module.cmake
@@ -2,7 +2,7 @@ set(DOCUMENTATION "The modules provides filters may be particularly useful for
 ultrasound image reconstruction and analysis.")
 
 # A library is only created when FFTW support is ON
-set(_fft_depends COMPILE_DEPENDS ITKFFT)
+set(_fft_depends ITKFFT)
 if(ITK_USE_FFTWF OR ITK_USE_FFTWD)
   set(_fft_depends ITKFFT)
 endif()
@@ -18,6 +18,7 @@ itk_module(Ultrasound
     ITKTransform
     ITKTestKernel
     ITKImageSources
+    ${_fft_depends}
   EXCLUDE_FROM_DEFAULT
   DESCRIPTION
     "${DOCUMENTATION}"
