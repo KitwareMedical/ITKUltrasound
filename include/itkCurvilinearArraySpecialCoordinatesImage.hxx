ea59ea68e5cd518bab8f8139cd1d3241281de7eb
Matt McCormick <matt.mccormick@kitware.com>
2018-04-19 11:07:05 -0400

    BUG: Expand casts for CurvilinearArraySpecialCoordinatesImage
    In BModeImageFilter, there are ImageToImageFilter's that CopyInformation from
    Image< TPixel, Dimension > and CurvilinearArraySpecialCoordinatesImage
    < TOtherPixel, Dimension >. Handle these cases.

--- include/itkCurvilinearArraySpecialCoordinatesImage.hxx
+++ include/itkCurvilinearArraySpecialCoordinatesImage.hxx
@@ -29,6 +29,8 @@
 #define itkCurvilinearArraySpecialCoordinatesImage_hxx
 #include "itkCurvilinearArraySpecialCoordinatesImage.h"
 
+#include <complex>
+
 namespace itk
 {
 
@@ -62,14 +64,120 @@ CurvilinearArraySpecialCoordinatesImage< TPixel, VDimension >
 
   if ( data )
     {
-    // Attempt to cast data to an ImageBase
-    const CurvilinearArraySpecialCoordinatesImage< TPixel, VDimension > * const curvilinearArray = dynamic_cast< const CurvilinearArraySpecialCoordinatesImage< TPixel, VDimension > * >( data );
-
+    // Attempt to cast data to an CurvilinearArraySpecialCoordinatesImage
+    const CurvilinearArraySpecialCoordinatesImage< TPixel, VDimension > * const curvilinearArray =
+      dynamic_cast< const CurvilinearArraySpecialCoordinatesImage< TPixel, VDimension > * >( data );
+    const CurvilinearArraySpecialCoordinatesImage< unsigned char, VDimension > * const curvilinearArrayUnsignedChar =
+      dynamic_cast< const CurvilinearArraySpecialCoordinatesImage< unsigned char, VDimension > * >( data );
+    const CurvilinearArraySpecialCoordinatesImage< signed char, VDimension > * const curvilinearArraySignedChar =
+      dynamic_cast< const CurvilinearArraySpecialCoordinatesImage< signed char, VDimension > * >( data );
+    const CurvilinearArraySpecialCoordinatesImage< unsigned short, VDimension > * const curvilinearArrayUnsignedShort =
+      dynamic_cast< const CurvilinearArraySpecialCoordinatesImage< unsigned short, VDimension > * >( data );
+    const CurvilinearArraySpecialCoordinatesImage< short, VDimension > * const curvilinearArrayShort =
+      dynamic_cast< const CurvilinearArraySpecialCoordinatesImage< short, VDimension > * >( data );
+    const CurvilinearArraySpecialCoordinatesImage< unsigned int, VDimension > * const curvilinearArrayUnsignedInt =
+      dynamic_cast< const CurvilinearArraySpecialCoordinatesImage< unsigned int, VDimension > * >( data );
+    const CurvilinearArraySpecialCoordinatesImage< int, VDimension > * const curvilinearArrayInt =
+      dynamic_cast< const CurvilinearArraySpecialCoordinatesImage< int, VDimension > * >( data );
+    const CurvilinearArraySpecialCoordinatesImage< unsigned long long, VDimension > * const curvilinearArrayUnsignedLongLong =
+      dynamic_cast< const CurvilinearArraySpecialCoordinatesImage< unsigned long long, VDimension > * >( data );
+    const CurvilinearArraySpecialCoordinatesImage< long long, VDimension > * const curvilinearArrayLongLong =
+      dynamic_cast< const CurvilinearArraySpecialCoordinatesImage< long long, VDimension > * >( data );
+    const CurvilinearArraySpecialCoordinatesImage< float, VDimension > * const curvilinearArrayFloat =
+      dynamic_cast< const CurvilinearArraySpecialCoordinatesImage< float, VDimension > * >( data );
+    const CurvilinearArraySpecialCoordinatesImage< double, VDimension > * const curvilinearArrayDouble =
+      dynamic_cast< const CurvilinearArraySpecialCoordinatesImage< double, VDimension > * >( data );
+    const CurvilinearArraySpecialCoordinatesImage< std::complex< float >, VDimension > * const curvilinearArrayComplexFloat =
+      dynamic_cast< const CurvilinearArraySpecialCoordinatesImage< std::complex< float >, VDimension > * >( data );
+    const CurvilinearArraySpecialCoordinatesImage< std::complex< double >, VDimension > * const curvilinearArrayComplexDouble =
+      dynamic_cast< const CurvilinearArraySpecialCoordinatesImage< std::complex< double >, VDimension > * >( data );
     if ( curvilinearArray != ITK_NULLPTR )
       {
       this->SetLateralAngularSeparation( curvilinearArray->GetLateralAngularSeparation() );
       this->SetRadiusSampleSize( curvilinearArray->GetRadiusSampleSize() );
       this->SetFirstSampleDistance( curvilinearArray->GetFirstSampleDistance() );
+      }
+    else if ( curvilinearArray != ITK_NULLPTR )
+      {
+      this->SetLateralAngularSeparation( curvilinearArray->GetLateralAngularSeparation() );
+      this->SetRadiusSampleSize( curvilinearArray->GetRadiusSampleSize() );
+      this->SetFirstSampleDistance( curvilinearArray->GetFirstSampleDistance() );
+      }
+    else if ( curvilinearArrayUnsignedChar != ITK_NULLPTR )
+      {
+      this->SetLateralAngularSeparation( curvilinearArrayUnsignedChar->GetLateralAngularSeparation() );
+      this->SetRadiusSampleSize( curvilinearArrayUnsignedChar->GetRadiusSampleSize() );
+      this->SetFirstSampleDistance( curvilinearArrayUnsignedChar->GetFirstSampleDistance() );
+      }
+    else if ( curvilinearArraySignedChar != ITK_NULLPTR )
+      {
+      this->SetLateralAngularSeparation( curvilinearArraySignedChar->GetLateralAngularSeparation() );
+      this->SetRadiusSampleSize( curvilinearArraySignedChar->GetRadiusSampleSize() );
+      this->SetFirstSampleDistance( curvilinearArraySignedChar->GetFirstSampleDistance() );
+      }
+    else if ( curvilinearArrayUnsignedShort != ITK_NULLPTR )
+      {
+      this->SetLateralAngularSeparation( curvilinearArrayUnsignedShort->GetLateralAngularSeparation() );
+      this->SetRadiusSampleSize( curvilinearArrayUnsignedShort->GetRadiusSampleSize() );
+      this->SetFirstSampleDistance( curvilinearArrayUnsignedShort->GetFirstSampleDistance() );
+      }
+    else if ( curvilinearArrayShort != ITK_NULLPTR )
+      {
+      this->SetLateralAngularSeparation( curvilinearArrayShort->GetLateralAngularSeparation() );
+      this->SetRadiusSampleSize( curvilinearArrayShort->GetRadiusSampleSize() );
+      this->SetFirstSampleDistance( curvilinearArrayShort->GetFirstSampleDistance() );
+      }
+    else if ( curvilinearArrayUnsignedInt != ITK_NULLPTR )
+      {
+      this->SetLateralAngularSeparation( curvilinearArrayUnsignedInt->GetLateralAngularSeparation() );
+      this->SetRadiusSampleSize( curvilinearArrayUnsignedInt->GetRadiusSampleSize() );
+      this->SetFirstSampleDistance( curvilinearArrayUnsignedInt->GetFirstSampleDistance() );
+      }
+    else if ( curvilinearArrayInt != ITK_NULLPTR )
+      {
+      this->SetLateralAngularSeparation( curvilinearArrayInt->GetLateralAngularSeparation() );
+      this->SetRadiusSampleSize( curvilinearArrayInt->GetRadiusSampleSize() );
+      this->SetFirstSampleDistance( curvilinearArrayInt->GetFirstSampleDistance() );
+      }
+    else if ( curvilinearArrayUnsignedLongLong != ITK_NULLPTR )
+      {
+      this->SetLateralAngularSeparation( curvilinearArrayUnsignedLongLong->GetLateralAngularSeparation() );
+      this->SetRadiusSampleSize( curvilinearArrayUnsignedLongLong->GetRadiusSampleSize() );
+      this->SetFirstSampleDistance( curvilinearArrayUnsignedLongLong->GetFirstSampleDistance() );
+      }
+    else if ( curvilinearArrayLongLong != ITK_NULLPTR )
+      {
+      this->SetLateralAngularSeparation( curvilinearArrayLongLong->GetLateralAngularSeparation() );
+      this->SetRadiusSampleSize( curvilinearArrayLongLong->GetRadiusSampleSize() );
+      this->SetFirstSampleDistance( curvilinearArrayLongLong->GetFirstSampleDistance() );
+      }
+    else if ( curvilinearArrayFloat != ITK_NULLPTR )
+      {
+      this->SetLateralAngularSeparation( curvilinearArrayFloat->GetLateralAngularSeparation() );
+      this->SetRadiusSampleSize( curvilinearArrayFloat->GetRadiusSampleSize() );
+      this->SetFirstSampleDistance( curvilinearArrayFloat->GetFirstSampleDistance() );
+      }
+    else if ( curvilinearArrayDouble != ITK_NULLPTR )
+      {
+      this->SetLateralAngularSeparation( curvilinearArrayDouble->GetLateralAngularSeparation() );
+      this->SetRadiusSampleSize( curvilinearArrayDouble->GetRadiusSampleSize() );
+      this->SetFirstSampleDistance( curvilinearArrayDouble->GetFirstSampleDistance() );
+      }
+    else if ( curvilinearArrayComplexFloat != ITK_NULLPTR )
+      {
+      this->SetLateralAngularSeparation( curvilinearArrayComplexFloat->GetLateralAngularSeparation() );
+      this->SetRadiusSampleSize( curvilinearArrayComplexFloat->GetRadiusSampleSize() );
+      this->SetFirstSampleDistance( curvilinearArrayComplexFloat->GetFirstSampleDistance() );
+      }
+    else if ( curvilinearArrayComplexDouble != ITK_NULLPTR )
+      {
+      this->SetLateralAngularSeparation( curvilinearArrayComplexDouble->GetLateralAngularSeparation() );
+      this->SetRadiusSampleSize( curvilinearArrayComplexDouble->GetRadiusSampleSize() );
+      this->SetFirstSampleDistance( curvilinearArrayComplexDouble->GetFirstSampleDistance() );
+      }
+    else if ( std::string(data->GetNameOfClass()) == "Image" )
+      {
+      // No extra meta data to transfer
       }
     else
       {
