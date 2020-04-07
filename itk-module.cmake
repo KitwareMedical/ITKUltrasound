b0b33fc6c89fcb885e4b0df1acfe55fa39690dc9
Dženan Zukić <dzenan.zukic@kitware.com>
2020-04-07 15:55:47 -0400

    COMP: renaming files to match the new naming pattern

--- itk-module.cmake
+++ itk-module.cmake
@@ -1,10 +1,9 @@
 set(DOCUMENTATION "The modules provides filters may be particularly useful for
 ultrasound image reconstruction and analysis.")
 
-# A library is only created when FFTW support is ON
 set(_fft_depends ITKFFT)
-if(ITK_USE_FFTWF OR ITK_USE_FFTWD)
-  set(_fft_depends ITKFFT)
+if(ITK_USE_GPU)
+  list(APPEND _fft_depends ITKGPUCommon) # also add OpenCL_FFT, and replace by clFFT
 endif()
 
 itk_module(Ultrasound
@@ -22,11 +21,8 @@ itk_module(Ultrasound
     Strain
   PRIVATE_DEPENDS
     ITKHDF5
-  TEST_DEPENDS
-    ITKTransform
     ITKTestKernel
     ITKImageSources
-    ${_fft_depends}
   EXCLUDE_FROM_DEFAULT
   ENABLE_SHARED
   DESCRIPTION
