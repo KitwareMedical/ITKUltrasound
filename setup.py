2bcabf826a3f20fe666f00e4835bedda67c6b5d0
Tom Birdsong <tom.birdsong@kitware.com>
2022-06-01 08:56:51 -0400

    ENH: Bump ITKUltrasound to 0.5.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from skbuild import setup
 
 setup(
     name='itk-ultrasound',
-    version='0.5.4',
+    version='0.5.5',
     author='Matthew McCormick',
     author_email='matt.mccormick@kitware.com',
     packages=['itk'],
